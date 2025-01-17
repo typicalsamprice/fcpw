e14a0aa4b90a3007d4f49f3a23cabb52c77a5d68
Sam Price <sam@typicalsamprice.com>
2025-01-17 14:37:33 -0600

    Adds features to test script

--- hyperfine.sh
+++ hyperfine.sh
@@ -1,1 +1,14 @@
-hyperfine --warmup 5 'cargo test -r -F pext perft -- --include-ignored' 'cargo test -r -F magic perft -- --include-ignored' 'cargo test -r perft -- --include-ignored'
+perft_features() {
+    printf "cargo test -r %s perft -- --include-ignored" "${1}"
+}
+
+hyperfine --warmup 5\
+    "$(perft_features "-F pext")"\
+    "$(perft_features "-F magic")"\
+    "$(perft_features)"\
+    "$(perft_features "-F inline")"\
+    "$(perft_features "-F inline-aggressive")"\
+    "$(perft_features "-F pext,inline")"\
+    "$(perft_features "-F pext,inline-aggressive")"\
+    "$(perft_features "-F magic,inline")"\
+    "$(perft_features "-F magic,inline-aggressive")"\
