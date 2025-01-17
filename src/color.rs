bc129c477fc38f99ae0b95eb1bb14f1689f267c3
Sam Price <sam@typicalsamprice.com>
2025-01-17 00:37:22 -0600

    Adds const-not to Color

--- src/color.rs
+++ src/color.rs
@@ -1,6 +1,6 @@
 use std::ops::Not;
 
-use crate::square::Rank;
+use crate::{square::Rank, Direction};
 
 #[derive(Debug, PartialEq, PartialOrd, Eq, Ord, Clone, Copy)]
 pub enum Color {
@@ -16,14 +16,27 @@ impl Color {
             Color::Black => unsafe { std::mem::transmute(7 - rank as u8) },
         }
     }
+
+    #[inline]
+    pub const fn forward(self) -> Direction {
+        match self {
+            Color::White => Direction::North,
+            Color::Black => Direction::South,
+        }
+    }
+
+    #[inline]
+    pub const fn not(self) -> Self {
+        match self {
+            Color::White => Color::Black,
+            Color::Black => Color::White,
+        }
+    }
 }
 
 impl Not for Color {
     type Output = Self;
     fn not(self) -> Self::Output {
-        match self {
-            Color::White => Color::Black,
-            Color::Black => Color::White,
-        }
+        self.not()
     }
 }
